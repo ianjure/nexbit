c5d9c8d812f4397558ac1b41a467b52f231afc20
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-11-27 20:33:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ import streamlit as st
 st.set_page_config(page_title="Nexbit", page_icon="🪙")
 
 st.write("nexbit")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write("Bitcoin")
+with col2:
+    with st.popover("Change Crypto"):
+        st.markdown("Hello World 👋")
+        name = st.text_input("What's your name?")
+with col3:
+    st.metric("Prediction", "70 °F", "1.2 °F")
