58ee3fb34fc41048c4d379a1a31828717247f644
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-03 11:48:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ primary = """
     </style>
         """
 st.markdown(primary, unsafe_allow_html=True)
-"""
+
 # [STREAMLIT] ADJUST SETTINGS BUTTON
-set_btn = ""
+set_btn = """
     <style>
     [class="st-emotion-cache-11t4yo9 ef3psqc19"] {
         border-radius: 5rem;
@@ -104,9 +104,9 @@ set_btn = ""
         height: 3rem;
     }
     </style>
-        ""
+        """
 st.markdown(set_btn, unsafe_allow_html=True)
-"""
+
 
 # [STREAMLIT] SESSION STATE FOR CRYPTO SELECTED
 if "crypto" not in st.session_state:
