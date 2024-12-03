9dabab023d2e5c929e0073ec3ca4ebfd48d2e5fc
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-03 12:27:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with info:
     # CRYPTO INFO
     logo, name = st.columns([1,3])
     with logo:
-        st.image('icon.png')
+        st.image("logo.svg")
     with name:
         st.markdown(f"<h1 style='text-align: left; font-size: 3.5rem; font-weight: 900; line-height: 0.5;'>{st.session_state.crypto}</h1>", unsafe_allow_html=True)
     symbol, supply = st.columns([1,3])
