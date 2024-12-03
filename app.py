65fb3131b0364fea11599335573abfc24d3420b5
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-03 16:44:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -153,8 +153,12 @@ with info:
     # MODEL PREDICTION
     increase = """
         <div style='width: auto; height: auto; padding: 12px; margin: 0px; margin-bottom: 15px; border: 2px solid #AFFD86; border-radius: 0.8rem; background-color: #8DFB4E1A;'>
-            <span style='text-align: left; font-size: 0.8rem; font-weight: 500;'>The price will increase with 67% confidence.</span>
-            <span style='text-align: right;' class='material-symbols-rounded'>⬆️</span>
+            <div>
+                <span style='text-align: left; font-size: 0.8rem; font-weight: 500;'>The price will increase with 67% confidence.</span>
+            </div>
+            <div>
+                <span style='text-align: left; font-size: 0.8rem; font-weight: 500;'>Model Accuracy: 54%</span>
+            </div>
         </div>
         """
     st.markdown(increase, unsafe_allow_html=True)
