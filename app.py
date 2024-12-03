9b6219819457abd3b9490296a02d4c5ab6509157
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-03 10:54:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,9 @@ st.markdown(hide_menu, unsafe_allow_html=True)
 # [STREAMLIT] CENTER SEGMENTED CONTROL
 seg_cent = """
     <style>
+    [class="stElementContainer element-container st-emotion-cache-1c6qvx0 e1f1d6gn4"] {
+        justify-items: center;
+    }
     [data-baseweb="button-group"] {
         margin: 0;
     }
