f7ec9af49427e74c5296c7fbf68dfa6b6d5699cb
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-04 09:36:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ st.markdown(
         font-size: 3.5rem;
         font-weight: 600;
         line-height: 0.8;
-        padding-top: 3px;
+        padding-top: -15px;
         color: white;
     }
     </style>
@@ -311,7 +311,7 @@ with info:
                 updateCounter();
             })();
         </script>
-        """, height=50)
+        """, height=0)
     # MODEL PREDICTION
     date_acc = f"""
         <div style='display: flex; justify-content: space-between; align-items: center; margin-bottom: 10px;'>
