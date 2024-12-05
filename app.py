081bf4e696284f2bca85efac01feb36bca071188
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-05 11:24:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -506,7 +506,8 @@ with sentiment_section:
     with chart_1:
         AV_chart = alt.Chart(sentiment_counts_AV).mark_bar().encode(
             x='count:Q',
-            y="sentiment:O"
+            y=alt.Y('sentiment:O', axis=alt.Axis(title='Sentiment', labelAngle=0)),
+            color='sentiment:N'
         ).properties(height=100, width='container', title='Alpha Vantage')
         st.altair_chart(AV_chart, use_container_width=True)
     with chart_2:
