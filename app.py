645fdb73a94f51d030d97a2aed89f070c134174e
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-05 21:40:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -894,7 +894,7 @@ with news_section:
     heatmap = alt.Chart(daily_sentiment).mark_rect().encode(
         alt.X("date(date):O").axis(format="%e", labelAngle=0, title=None),
         alt.Y("month(date):O").axis(title=None),
-        alt.Color("sentiment:Q", title=None, scale=alt.Scale(domain=[max_sentiment, upper_mid_sentiment, lower_mid_sentiment, min_sentiment], range=[f"{color1_light}", f"{black_light}", f"{black_light}", f"{color2_light}"]),
+        alt.Color("sentiment:Q", title=None, scale=alt.Scale(domain=[min_sentiment, lower_mid_sentiment, upper_mid_sentiment, max_sentiment], range=[f"{color2_light}", f"{black_light}", f"{black_light}", f"{color1_light}"]),
                   legend=alt.Legend(padding=0, labelFontSize=10)),
         tooltip=[
             alt.Tooltip("date(date):T", title="Date"),
