19704d7200a90d79cdd212c290c77c687c4c059b
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-06 09:28:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -984,9 +984,11 @@ with news_section:
     st.markdown(about_title, unsafe_allow_html=True)
 
     about = f"""
-        <span style='text-align: justify; font-size: 0.9rem; font-weight: 300; color: {text_dark}; margin-top: -10px;'>
-            Bitcoin (BTC) is the first cryptocurrency built on blockchain technology, also known as a decentralized digital currency that is based on cryptography. Unlike government-issued or fiat currencies such as US Dollars or Euro which are controlled by central banks, Bitcoin can operate without the need of a central authority like a central bank or a company. The decentralized nature allows it to operate on a peer-to-peer network whereby users are able to send funds to each other without going through intermediaries.
-        </span>
+        <div style='text-align: justify;'>
+            <p style='text-align: justify; font-size: 0.9rem; font-weight: 300; color: {text_dark}; margin-top: -10px;'>
+                Bitcoin (BTC) is the first cryptocurrency built on blockchain technology, also known as a decentralized digital currency that is based on cryptography. Unlike government-issued or fiat currencies such as US Dollars or Euro which are controlled by central banks, Bitcoin can operate without the need of a central authority like a central bank or a company. The decentralized nature allows it to operate on a peer-to-peer network whereby users are able to send funds to each other without going through intermediaries.
+            </p>
+        </div>
         """
     st.markdown(about, unsafe_allow_html=True)
 
