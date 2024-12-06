d3a4fd361a59184b401a8c1846141556abd7d3a5
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-12-06 08:58:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ info_hover = """
         display: none;
         cursor: default;
         position: absolute;
-        top: 100%;
+        top: 105%;
         left: 50%;
         transform: translateX(-50%);
         background-color: """ + black_light + """;
